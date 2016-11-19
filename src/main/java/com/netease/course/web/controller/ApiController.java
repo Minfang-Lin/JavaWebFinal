d8e11c46437234c7a5bbc9a97f04018be4f743b5
Minfang-Lin <leafcard@gmail.com>
2016-11-19 16:15:31 +0800

    fix filepath

--- src/main/java/com/netease/course/web/controller/ApiController.java
+++ src/main/java/com/netease/course/web/controller/ApiController.java
@@ -16,12 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.netease.course.model.Buy;
 import com.netease.course.model.SettleAccount;
 import com.netease.course.model.User;
 import com.netease.course.service.impl.ProductService;
 import com.netease.course.service.impl.UserService;
-
 
 @Controller
 @RequestMapping("/api")
@@ -83,19 +84,20 @@ public class ApiController {
 	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public Map<String, Object> uploadProductImage(@RequestParam MultipartFile file, HttpServletRequest request) {
+	public Map<String, Object> uploadProductImage(HttpServletRequest request) {
+		MultipartFile file = ((MultipartHttpServletRequest) request).getFile("file");
 		Map<String, Object> map = new HashMap<String, Object>();
-		String fileName = file.getOriginalFilename();
-		String filePath = request.getSession().getServletContext().getRealPath("/image");		
-		File tempFile = new File(filePath, fileName); 
-		if(!tempFile.exists()){  
-			tempFile.mkdirs();  
-        } 
-        try {
+		String fileName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+		String filePath = request.getSession().getServletContext().getRealPath("/image");
+		File tempFile = new File(filePath, fileName);
+		if (!tempFile.exists()) {
+			tempFile.mkdirs();
+		}
+		try {
 			file.transferTo(tempFile);
 			map.put("code", 200);
 			map.put("message", "上传成功");
-			map.put("result", request.getContextPath()+"/image/"+fileName);			
+			map.put("result", request.getServletContext().getRealPath("/") + "image/" + fileName);
 		} catch (IllegalStateException e) {
 			map.put("code", 403);
 			map.put("message", "上传失败");
@@ -104,7 +106,7 @@ public class ApiController {
 		} catch (IOException e) {
 			map.put("code", 403);
 			map.put("message", "上传失败");
-			map.put("result", null);		
+			map.put("result", null);
 			e.printStackTrace();
 		}
 		return map;
